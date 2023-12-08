583353ac58725c58fd65977c09e111ede007fa72
vaibarua <vaishalibarua@publicissapient.com>
2023-12-08 09:21:09 +0530

    Added RBI interface and created classes for Banks.

--- src/main/java/RBI.java
+++ src/main/java/RBI.java
@@ -1,43 +1,12 @@
-public class RBI {
-    float balance;
+import java.io.BufferedReader;
+import java.util.*;
 
-    public RBI() {
-        balance = 1000.0F;
-    }
-
-    public void depositMoney(float amount, Customer obj) { //pass Customer object later
-        obj.balance = obj.balance + amount;
-        System.out.println("You have successfully deposited the amount. Current balance is :"+obj.balance);
-    }
-    public void withdrawMoney(float amount, Customer obj){ //pass Customer object later
-        if(amount>obj.balance)
-        {
-            System.out.println("You do not have enough balance to Withdraw");
-        }
-        else
-        {
-            obj.balance = obj.balance - amount;
-            System.out.println("You have successfully withdrawn the amount. Current balance is :"+obj.balance);
-        }
-    }
-    public void openFD(float amount, float years) {
-        float ROI = 6.5F;
-        float maturedAmount=amount;
-        for(int i=1;i<=years;i++) {
-            maturedAmount+= ((ROI*maturedAmount)/100);
-            System.out.println("Amount after Year"+i+": "+maturedAmount);
-        }
-        System.out.println("Total Profit: "+(maturedAmount-amount));
-    }
-    public void applyLoan(String loanType, float amount, int years) {
-        float interest;
-        float ROI = 10;
-        interest = (amount*ROI*years)/100;
-        System.out.println("Total interest: "+interest);
-        System.out.println("Outstanding amount after interest: "+(amount+interest));
-    }
-    public void applyCreditCard() {}
-    public float getBalance() {
-        return 0.0f;
-    }
+public interface RBI {
+    final int minBalance = 1000;
+    void depositMoney(float amount,Customer obj);
+    void withdrawMoney(float amount, Customer obj);
+    void openFD(float amount,int years);
+    void applyLoan(BufferedReader br, float amount, int years);
+    void applyCreditCard();
+    float getBalance();
 }
